a155011e506dac5486065d5f445d988681eb71e3
Bhuvanesh-IOT <Bhuvanes.Santhanam@motherson.com>
2026-05-07 15:05:51 +0530

    acess control added

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TooltipModule } from 'primeng/tooltip';
 import { CommonModule } from '@angular/common';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,4 +11,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.scss'],
 })
-export class SidebarComponent {}
+export class SidebarComponent implements OnInit {
+  userType: string = '';
+
+  constructor(private cookieService: CookieService) {}
+
+  ngOnInit() {
+    this.userType = this.cookieService.get('Usertype') || '';
+  }
+
+  get isQA() {
+    return this.userType === 'QA' || this.isAdmin;
+  }
+
+  get isIMS() {
+    return this.userType === 'IMS' || this.isAdmin;
+  }
+
+  get isAdmin() {
+    return this.userType === 'BASE-ADMIN' || this.userType === 'ADMIN';
+  }
+}
