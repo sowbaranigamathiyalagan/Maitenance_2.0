9173d8fd3542fecad28a568ac1b00e64d44be747
Rajan <Prithivi.Rajan@mate.motherson.com>
2026-05-09 09:39:43 +0530

    location filter bug

--- Maitenance_2.0-frontend/src/app/modules/overview/Addons/toolbox-list/toolbox-list.ts
+++ Maitenance_2.0-frontend/src/app/modules/overview/Addons/toolbox-list/toolbox-list.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Intimation as IntimationService } from '../../../../core/services/intimation';
-
+ 
 @Component({
   selector: 'app-toolbox-list',
   standalone: true,
@@ -18,17 +18,17 @@ export class ToolboxListComponent implements OnInit {
   pageSize = 15;
   filterStatus: string = 'all';
   filterLocation: string = 'all';
-
+ 
   constructor(
     private intimationService: IntimationService,
     private cdr: ChangeDetectorRef,
     private zone: NgZone
   ) {}
-
+ 
   ngOnInit() {
     this.fetchToolStatus();
   }
-
+ 
   fetchToolStatus() {
     this.loading = true;
     this.hasError = false;
@@ -51,31 +51,31 @@ export class ToolboxListComponent implements OnInit {
       }
     });
   }
-
+ 
   onFilterChange() {
     this.currentPage = 1;
     this.cdr.detectChanges();
   }
-
+ 
   get filteredToolboxes() {
   if (!this.toolboxes) return [];
-
+ 
   return this.toolboxes.filter(box => {
-
+ 
     const statusMatch =
       this.filterStatus === 'all' ||
       (box.status &&
         box.status.toLowerCase() === this.filterStatus.toLowerCase());
-
+ 
     const locationMatch =
       this.filterLocation === 'all' ||
       (box.location &&
         box.location.toLowerCase() === this.filterLocation.toLowerCase());
-
+ 
     return statusMatch && locationMatch;
   });
 }
-
+ 
   getStatusClass(status: string) {
     return {
       critical: status === 'critical',
@@ -83,30 +83,30 @@ export class ToolboxListComponent implements OnInit {
       ok: status === 'ok'
     };
   }
-
+ 
   get paginatedData() {
     const data = this.filteredToolboxes;
     const start = (this.currentPage - 1) * this.pageSize;
     return data.slice(start, start + this.pageSize);
   }
-
+ 
   get pages() {
     const totalItems = this.filteredToolboxes.length;
     return Array(Math.ceil(totalItems / this.pageSize))
       .fill(0)
       .map((_, i) => i + 1);
   }
-
+ 
   goToPage(page: number) {
     this.currentPage = page;
   }
-
+ 
   nextPage() {
     if (this.currentPage < this.pages.length) {
       this.currentPage++;
     }
   }
-
+ 
   prevPage() {
     if (this.currentPage > 1) {
       this.currentPage--;
